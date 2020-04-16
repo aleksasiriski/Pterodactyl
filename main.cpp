e0bddaeb83af501cd8ab47b08f66160de3da2db8
Aleksa Siriški <aleksasiriski@lavelektronik.com>
2020-04-16 11:39:48 +0200

    Server

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std;
-#include "database.hpp"
-#include "server.hpp"
-#include "node.hpp"
 #include "location.hpp"
 #include "nestandpack.hpp"
 #include "user.hpp"
--- server.hpp
+++ server.hpp
@@ -1,6 +1,48 @@
-#ifndef _HPP_INCLUDED
-#define _HPP_INCLUDED
+#ifndef SERVER_HPP_INCLUDED
+#define SERVER_HPP_INCLUDED
+#include "database.hpp"
+class Server()
+{
+private:
+	int CPU,RAM,ROM;
+	database DB;
+public:
+	Server()
+	{
+		CPU=1;
+		RAM=2;
+		ROM=5;
+	}
+	Server(int C,int RA,int RO)
+	{
+		CPU=C;
+		RAM=RA;
+		ROM=RO;
+	}
+	int getCPU()const
+	{
+		return CPU;
+	}
+	void setCPU(int C)
+	{
+		CPU=C;
+	}
+	int getRAM()const
+	{
+		return RAM;
+	}
+	int setRAM(int RA)
+	{
+		RAM=RA;
+	}
+	int getROM()const
+	{
+		return ROM;
+	}
+	int setROM(int RO)
+	{
+		ROM=RO;
+	}
+};
 
-
-
-#endif // _HPP_INCLUDED
+#endif // SERVER_HPP_INCLUDED
