0188ba00ed82f506e7ed0ecef12986f7df73817e
Aleksa Siriški <aleksasiriski@lavelektronik.com>
2020-04-16 11:56:18 +0200

    Node and Location

--- node.hpp
+++ node.hpp
@@ -1,6 +1,49 @@
-#ifndef _HPP_INCLUDED
-#define _HPP_INCLUDED
+#ifndef NODE_HPP_INCLUDED
+#define NODE_HPP_INCLUDED
+#include "server.hpp"
 
+class Node()
+{
+private:
+	int ACPU,ARAM,AROM;
+	server S;
+public:
+	Node()
+	{
+		ACPU=2;
+		ARAM=16;
+		AROM=100;
+	}
+	Node(int AC,int ARA,int ARO)
+	{
+		ACPU=AC;
+		ARAM=ARA;
+		AROM=ARO;
+	}
+	int getACPU()const
+	{
+		return ACPU;
+	}
+	void setACPU(int AC)
+	{
+		ACPU=AC;
+	}
+	int getARAM()const
+	{
+		return ARAM;
+	}
+	int setARAM(int ARA)
+	{
+		ARAM=ARA;
+	}
+	int getAROM()const
+	{
+		return AROM;
+	}
+	int setAROM(int ARO)
+	{
+		AROM=ARO;
+	}
+};
 
-
-#endif // _HPP_INCLUDED
+#endif // NODE_HPP_INCLUDED
