5c13b987bb6a939f8e9645fd43f12668d9d45682
Aleksa Siriški <aleksasiriski@lavelektronik.com>
2020-04-17 17:56:28 +0200

    Update node.hpp

--- node.hpp
+++ node.hpp
@@ -13,12 +13,28 @@ public:
 		ACPU=2;
 		ARAM=16;
 		AROM=100;
+		S.setCPU(1);
+		S.setRAM(2);
+		S.setROM(5);
+		DB.setDB("","","","");
 	}
-	Node(int AC,int ARA,int ARO)
+	Node(int AC,int ARA,int ARO,int C,int RA,int RO,string dbname,string dbip,string dbuser,string dbpassword)
 	{
 		ACPU=AC;
 		ARAM=ARA;
 		AROM=ARO;
+		S.setCPU(C);
+		S.setRAM(RA);
+		S.setROM(RO);
+		DB.setDB(dbname,dbip,dbuser,dbpassword);
+	}
+	string getDB()const
+	{
+		return S.getDB();
+	}
+	void setDB(string dbname,string dbip,string dbuser,string dbpassword)
+	{
+		S.setDB(dbname,dbip,dbuser,dbpassword);
 	}
 	int getACPU()const
 	{
