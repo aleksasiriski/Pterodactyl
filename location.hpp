391050d8bcdea1cdd2cd5298a94e65b085f53e45
Aleksa Siriški <aleksasiriski@lavelektronik.com>
2020-04-18 23:03:56 +0200

    Location

--- location.hpp
+++ location.hpp
@@ -24,6 +24,62 @@ public:
 	{
 		return CC;
 	}
+	string getDB()const
+	{
+		return N.getDB();
+	}
+	void setDB(string dbname,string dbip,string dbuser,string dbpassword)
+	{
+		N.setDB(dbname,dbip,dbuser,dbpassword);
+	}
+	int getCPU()const
+	{
+		return N.getCPU;
+	}
+	void setCPU(int C)
+	{
+		N.setCPU(C);
+	}
+	int getRAM()const
+	{
+		return N.getRAM;
+	}
+	int setRAM(int RA)
+	{
+		N.setRAM(RA);
+	}
+	int getROM()const
+	{
+		return N.getROM;
+	}
+	int setROM(int RO)
+	{
+		N.setROM(RO);
+	}
+	int getACPU()const
+	{
+		return N.getACPU();
+	}
+	void setACPU(int AC)
+	{
+		N.setACPU(AC);
+	}
+	int getARAM()const
+	{
+		return N.getARAM();
+	}
+	int setARAM(int ARA)
+	{
+		N.setARAM(ARA);
+	}
+	int getAROM()const
+	{
+		return N.setAROM();
+	}
+	int setAROM(int ARO)
+	{
+		N.setAROM(ARO);
+	}
 };
 
 #endif // LOCATION_HPP_INCLUDED
--- node.hpp
+++ node.hpp
@@ -16,7 +16,7 @@ public:
 		S.setCPU(1);
 		S.setRAM(2);
 		S.setROM(5);
-		DB.setDB("","","","");
+		S.setDB("","","","");
 	}
 	Node(int AC,int ARA,int ARO,int C,int RA,int RO,string dbname,string dbip,string dbuser,string dbpassword)
 	{
@@ -26,7 +26,7 @@ public:
 		S.setCPU(C);
 		S.setRAM(RA);
 		S.setROM(RO);
-		DB.setDB(dbname,dbip,dbuser,dbpassword);
+		S.setDB(dbname,dbip,dbuser,dbpassword);
 	}
 	string getDB()const
 	{
@@ -35,6 +35,30 @@ public:
 	void setDB(string dbname,string dbip,string dbuser,string dbpassword)
 	{
 		S.setDB(dbname,dbip,dbuser,dbpassword);
+	}
+	int getCPU()const
+	{
+		return S.getCPU;
+	}
+	void setCPU(int C)
+	{
+		S.setCPU(C);
+	}
+	int getRAM()const
+	{
+		return S.getRAM;
+	}
+	int setRAM(int RA)
+	{
+		S.setRAM(RA);
+	}
+	int getROM()const
+	{
+		return S.getROM;
+	}
+	int setROM(int RO)
+	{
+		S.setROM(RO);
 	}
 	int getACPU()const
 	{
