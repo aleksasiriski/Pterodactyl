fd30420faeae123c4e31fffa9b076fa87eba30e2
aleksasiriski <siriskialeksasiriski@gmail.com>
2020-05-14 23:25:47 +0200

    sneakpeek

--- main.cpp
+++ main.cpp
@@ -1,58 +1,10 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#define MAX 100
-using namespace std;
-#include "location.hpp"
-#include "eggnestpack.hpp"
-#include "user.hpp"
+#include "panel.hpp"
 unsigned short Admin::globalID=0;
-const string userfile="users.txt",adminfile="admins.txt";
-void startprogram()
-{
-	loadUsers(userfile);
-	loadAdmins(adminfile);
-	Admin::globalID=(*(admins.rbegin()))->getadminID();
-}
-void endprogram()
-{
-	saveUsers(userfile);
-	saveAdmins(adminfile);
-}
 int main()
 {
 	srand(time(NULL)); //seed za rand()
-    startprogram();
-
-    listUsers();
-    cout << endl;
-    listAdmins();
-
-	Location First;
-
-	Database db;
-	cout << db;
-    Server s;
-    cout << s;
-    Node n;
-    cout << n;
-    Location l;
-    cout << l;
-
-	User u;
-	setup(&u);
-	cout << u;
-	cout << u.resetPassword() << endl;
-	u.changePassword();
-
-	Admin a;
-	setup(&a);
-	cout << a;
-	//cout << Aleksa.resetPassword() << endl;
-	//Aleksa.changePassword();
+	Panel p;
+	p.CreateUser();
 
 	/*funkcionalnost 1: napraviti u lokaciju, node i dodeliti server obicnom useru*/
 
@@ -63,7 +15,5 @@ int main()
 
 	/*funkcionalnost 4: dodati novi node u istoj lokaciji sa jos jednim serverom i dodeliti ga
 	nekom useru, a zatim nakon provere da funkcionise obrisati server i user a ostaviti node u memoriji*/
-
-	endprogram();
 	return 0;
 }
